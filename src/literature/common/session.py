f73b975ff57b5fc0fffd01dfb2aa764af0439c6f
vivienho <56025826+vivienho@users.noreply.github.com>
2025-04-29 11:45:16 +0100

    feat: update SparkSession creation to allow sparknlp usage

--- src/literature/common/session.py
+++ src/literature/common/session.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession
 
 
@@ -12,15 +13,26 @@ class Session:
             self: Session,
             spark_uri: str = "local[*]",
             app_name: str = "ot-literature",
+            for_nlp: bool = False
     ) -> None:
         """Initialises Spark session.
         
             Args:
                 spark_uri (str): Spark URI. Defaults to "local[*]".
                 app_name (str): Spark application name. Defaults to "ot-literature".
+                for_nlp (bool): Whether session is used for sparknlp. Defaults to False.
         """
+        config=SparkConf()
+
+        if for_nlp:
+            config = (
+                SparkConf()
+                .set("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.12:5.0.0")
+            )
+
         self.spark = (
             SparkSession.Builder()
+            .config(conf=config)
             .master(spark_uri)
             .appName(app_name)
             .getOrCreate()
