176e532133faf1d010ea43a8ddab5f3d9823346f
vivienho <56025826+vivienho@users.noreply.github.com>
2025-07-03 11:46:02 +0100

    feat(ontoma): add extract_disease_curation function

--- src/literature/method/ontoma/index_parsers.py
+++ src/literature/method/ontoma/index_parsers.py
@@ -6,13 +6,19 @@ from typing import TYPE_CHECKING
 
 import pyspark.sql.functions as f
 
+from src.literature.method.ontoma.utils import (
+    translate_special_characters,
+    clean_disease_label
+)
+
 if TYPE_CHECKING:
     from pyspark.sql import Column, DataFrame
 
 __all__ = [
     "extract_disease_entities",
     "extract_target_entities",
-    "extract_drug_entities"
+    "extract_drug_entities",
+    "extract_disease_curation"
 ]
 
 
@@ -239,6 +245,64 @@ def extract_drug_entities(drug_index: DataFrame) -> DataFrame:
             f.lit("CD").alias("entityType")
         )
         # cleanup
+        .filter((f.col("entityLabel").isNotNull()) & (f.length("entityLabel") > 0))
+        .distinct()
+    )
+
+def extract_disease_curation(disease_curation: DataFrame) -> DataFrame:
+    """Process a disease curation table to extract disease entities.
+
+    It is expected that the disease curation table contains the SEMANTIC_TAG and PROPERTY_VALUE fields
+    as in the format mentioned here: https://github.com/opentargets/curation/blob/master/mappings/disease/README.md
+
+    Args:
+        disease_curation (DataFrame): DataFrame with the disease curation.
+    
+    Returns:
+        DataFrame: DataFrame with the extracted disease entities.
+    """
+    return (
+        disease_curation
+        # extract entities from relevant fields and annotate entity with score and nlpPipelineTrack
+        .select(
+            f.regexp_extract(
+                f.col("SEMANTIC_TAG"), r'^http.+/(\w+_\w+)$', 1
+            ).alias("entityId"),
+            _annotate_entity(
+                f.array(f.col("PROPERTY_VALUE")), 1.0, "term"
+            ).alias("curationTerm"),
+            _annotate_entity(
+                f.array(f.col("PROPERTY_VALUE")), 1.0, "symbol"
+            ).alias("curationSymbol")
+        )
+        # flatten and explode array of structs
+        .withColumn(
+            "entity",
+            f.explode(
+                f.flatten(
+                    f.array(
+                        f.col("curationTerm"),
+                        f.col("curationSymbol")
+                    )
+                )
+            )
+        )
+        # select relevant fields and specify entity type
+        .select(
+            f.col("entityId"),
+            clean_disease_label(
+                translate_special_characters(
+                    f.trim(
+                        f.col("entity.entityLabel")
+                    )
+                )
+            ).alias("entityLabel"),
+            f.col("entity.entityScore").alias("entityScore"),
+            f.col("entity.nlpPipelineTrack").alias("nlpPipelineTrack"),
+            f.lit("DS").alias("entityType")
+        )
+        # cleanup
+        .filter((f.col("entityId").isNotNull()) & (f.length("entityId") > 0))
         .filter((f.col("entityLabel").isNotNull()) & (f.length("entityLabel") > 0))
         .distinct()
     )
