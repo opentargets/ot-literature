d3e5c94ac6dcf064412e5dd2fc8f97e2b803509e
vivienho <56025826+vivienho@users.noreply.github.com>
2025-07-17 16:19:50 +0100

    refactor(ontoma): add OpenTargetsDrug class

---
+++ src/literature/method/ontoma/datasource/drug.py
@@ -0,0 +1,183 @@
+"""Open Targets drug index."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pyspark.sql.functions as f
+
+from src.literature.method.ontoma.utils import (
+    annotate_entity,
+    translate_special_characters
+)
+
+if TYPE_CHECKING:
+    from pyspark.sql import DataFrame
+
+
+class OpenTargetsDrug:
+    """Class to extract drug entities from the Open Targets drug index."""
+
+    @classmethod
+    def as_drug_label_lut(
+        cls: type[OpenTargetsDrug], 
+        drug_index: DataFrame
+    ) -> DataFrame:
+        """Generate drug label lookup table from the Open Targets drug index.
+        
+        Args:
+            drug_index (DataFrame): Open Targets drug index.
+
+        Returns:
+            DataFrame: Drug label lookup table.
+        """
+        return (
+            drug_index
+            # filter crossReferences for sources that have labels
+            .withColumn(
+                "crossReferences", 
+                f.filter(
+                    f.col("crossReferences"),
+                    lambda x: x["source"].isin("DailyMed", "USAN", "EMA")
+                )
+            )
+            # transform array of structs to array of strings and format ids
+            .withColumn(
+                "crossReferences",
+                f.transform(
+                    f.col("crossReferences"),
+                    lambda x: f.when(
+                        # if it's a DailyMed or USAN id, replace spaces encoded as "%20"
+                        x["source"].isin("DailyMed", "USAN"),
+                        f.transform(x["ids"], lambda i: f.regexp_replace(i, "%20", " "))
+                    ).when(
+                        # if it's an EMA id, extract the last part
+                        x["source"] == "EMA",
+                        f.transform(x["ids"], lambda i: f.regexp_extract(i, r'.+/EPAR/(.+)', 1))
+                    ).otherwise(x["ids"])
+                )
+            )
+            # extract entities from relevant fields and annotate entity with score and nlpPipelineTrack
+            .select(
+                f.col("id").alias("entityId"),
+                annotate_entity(
+                    f.array(f.col("name")), 1.0, "term"
+                ).alias("nameTerm"),
+                annotate_entity(
+                    f.array(f.col("name")), 1.0, "symbol"
+                ).alias("nameSymbol"),
+                annotate_entity(
+                    f.col("tradeNames"), 0.999, "term"
+                ).alias("tradeNamesTerm"),
+                annotate_entity(
+                    f.col("tradeNames"), 0.999, "symbol"
+                ).alias("tradeNamesSymbol"),
+                annotate_entity(
+                    f.col("synonyms"), 0.999, "term"
+                ).alias("synonymsTerm"),
+                annotate_entity(
+                    f.col("synonyms"), 0.999, "symbol"
+                ).alias("synonymsSymbol"),
+                annotate_entity(
+                    f.flatten(f.col("crossReferences")), 0.998, "term"
+                ).alias("crossReferencesTerm"),
+                annotate_entity(
+                    f.flatten(f.col("crossReferences")), 0.998, "symbol"
+                ).alias("crossReferencesSymbol")
+            )
+            # flatten and explode array of structs
+            .withColumn(
+                "entity",
+                f.explode(
+                    f.flatten(
+                        f.array(
+                            f.col("nameTerm"),
+                            f.col("nameSymbol"),
+                            f.col("tradeNamesTerm"),
+                            f.col("tradeNamesSymbol"),
+                            f.col("synonymsTerm"),
+                            f.col("synonymsSymbol"),
+                            f.col("crossReferencesTerm"),
+                            f.col("crossReferencesSymbol")
+                        )
+                    )
+                )
+            )
+            # select relevant fields and specify entity type
+            .select(
+                f.col("entityId"),
+                translate_special_characters(
+                    f.trim(
+                        f.col("entity.entityLabel")
+                    )
+                ).alias("entityLabel"),
+                f.col("entity.entityScore").alias("entityScore"),
+                f.col("entity.nlpPipelineTrack").alias("nlpPipelineTrack"),
+                f.lit("CD").alias("entityType")
+            )
+            # cleanup
+            .filter((f.col("entityLabel").isNotNull()) & (f.length("entityLabel") > 0))
+            .distinct()
+        )
+    
+    @classmethod
+    def as_drug_id_lut(
+        cls: type[OpenTargetsDrug], 
+        drug_index: DataFrame
+    ) -> DataFrame:
+        """Generate drug id lookup table from the Open Targets drug index.
+
+        Args:
+            drug_index (DataFrame): Open Targets drug index.
+
+        Returns:
+            DataFrame: Drug id lookup table.
+        """
+        return (
+            drug_index
+            # filter crossReferences for sources that have ids
+            .withColumn(
+                "crossReferences", 
+                f.filter(
+                    f.col("crossReferences"),
+                    lambda x: x["source"].isin("chEBI", "drugbank")
+                )
+            )
+            # transform array of structs to array of strings and format ids
+            .withColumn(
+                "crossReferences",
+                f.transform(
+                    f.col("crossReferences"),
+                    lambda x: f.when(
+                        # if it's a chEBI id, append "CHEBI" as a prefix
+                        x["source"] == "chEBI",
+                        f.concat(f.lit("CHEBI"), x["ids"][0])
+                    ).otherwise(x["ids"][0])
+                )
+            )
+            # extract entities from relevant fields and annotate entity with score and nlpPipelineTrack
+            .select(
+                f.col("id").alias("entityId"),
+                annotate_entity(
+                    f.col("crossReferences"), 1.0, "symbol"
+                ).alias("crossReferences")
+            )
+            # explode array of structs
+            .withColumn(
+                "entity",
+                f.explode(
+                    f.col("crossReferences"),
+                )
+            )
+            # select relevant fields and specify entity type
+            .select(
+                f.col("entityId"),
+                f.col("entity.entityLabel").alias("entityLabel"),
+                f.col("entity.entityScore").alias("entityScore"),
+                f.col("entity.nlpPipelineTrack").alias("nlpPipelineTrack"),
+                f.lit("CD").alias("entityType")
+            )
+            # cleanup
+            .filter((f.col("entityLabel").isNotNull()) & (f.length("entityLabel") > 0))
+            .distinct()
+        )
