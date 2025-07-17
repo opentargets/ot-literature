1a8d6bb58572214c11891df795c99561aa7dd378
vivienho <56025826+vivienho@users.noreply.github.com>
2025-07-17 15:27:13 +0100

    refactor(ontoma): add OpenTargetsDisease class

--- src/literature/method/ontoma/index_parsers.py
+++ src/literature/method/ontoma/index_parsers.py
@@ -9,9 +9,7 @@ import pyspark.sql.functions as f
 from src.literature.method.ontoma.utils import (
     annotate_entity,
     translate_special_characters,
-    clean_disease_label,
-    filter_disease_crossrefs,
-    format_disease_identifier
+    clean_disease_label
 )
 
 if TYPE_CHECKING:
@@ -26,88 +24,6 @@ __all__ = [
     "as_drug_id_lut"
 ]
 
-
-def extract_disease_entities(disease_index: DataFrame) -> DataFrame:
-    """Process the Open Targets disease index to extract disease entities.
-    
-    Args:
-        disease_index (DataFrame): DataFrame with the disease index.
-
-    Returns:
-        DataFrame: DataFrame with the extracted disease entities.
-    """
-    return (
-        disease_index
-        # extract entities from relevant fields and annotate entity with score and nlpPipelineTrack
-        .select(
-            f.col("id").alias("entityId"),
-            annotate_entity(
-                f.array(f.col("name")), 1.0, "term"
-            ).alias("nameTerm"),
-            annotate_entity(
-                f.array(f.col("name")), 1.0, "symbol"
-            ).alias("nameSymbol"),
-            annotate_entity(
-                f.col("synonyms.hasExactSynonym"), 0.999, "term"
-            ).alias("exactSynonymsTerm"),
-            annotate_entity(
-                f.col("synonyms.hasExactSynonym"), 0.999, "symbol"
-            ).alias("exactSynonymsSymbol"),
-            annotate_entity(
-                f.col("synonyms.hasNarrowSynonym"), 0.998, "term"
-            ).alias("narrowSynonymsTerm"),
-            annotate_entity(
-                f.col("synonyms.hasNarrowSynonym"), 0.998, "symbol"
-            ).alias("narrowSynonymsSymbol"),
-            annotate_entity(
-                f.col("synonyms.hasBroadSynonym"), 0.997, "term"
-            ).alias("broadSynonymsTerm"),
-            annotate_entity(
-                f.col("synonyms.hasBroadSynonym"), 0.997, "symbol"
-            ).alias("broadSynonymsSymbol"),
-            annotate_entity(
-                f.col("synonyms.hasRelatedSynonym"), 0.996, "term"
-            ).alias("relatedSynonymsTerm"),
-            annotate_entity(
-                f.col("synonyms.hasRelatedSynonym"), 0.996, "symbol"
-            ).alias("relatedSynonymsSymbol")
-        )
-        # flatten and explode array of structs
-        .withColumn(
-            "entity",
-            f.explode(
-                f.flatten(
-                    f.array(
-                        f.col("nameTerm"),
-                        f.col("nameSymbol"),
-                        f.col("exactSynonymsTerm"),
-                        f.col("exactSynonymsSymbol"),
-                        f.col("narrowSynonymsTerm"),
-                        f.col("narrowSynonymsSymbol"),
-                        f.col("broadSynonymsTerm"),
-                        f.col("broadSynonymsSymbol"),
-                        f.col("relatedSynonymsTerm"),
-                        f.col("relatedSynonymsSymbol")
-                    )
-                )
-            )
-        )
-        # select relevant fields and specify entity type
-        .select(
-            f.col("entityId"),
-            translate_special_characters(
-                f.trim(
-                    f.col("entity.entityLabel")
-                )
-            ).alias("entityLabel"),
-            f.col("entity.entityScore").alias("entityScore"),
-            f.col("entity.nlpPipelineTrack").alias("nlpPipelineTrack"),
-            f.lit("DS").alias("entityType")
-        )
-        # cleanup
-        .filter((f.col("entityLabel").isNotNull()) & (f.length("entityLabel") > 0))
-        .distinct()
-    )
 
 def extract_target_entities(target_index: DataFrame) -> DataFrame:
     """Process the Open Targets target index to extract target entities.
@@ -331,60 +247,6 @@ def extract_disease_curation(disease_curation: DataFrame) -> DataFrame:
         )
         # cleanup
         .filter((f.col("entityId").isNotNull()) & (f.length("entityId") > 0))
-        .filter((f.col("entityLabel").isNotNull()) & (f.length("entityLabel") > 0))
-        .distinct()
-    )
-
-def as_disease_id_lut(disease_index:DataFrame) -> DataFrame:
-    """Generate disease id lookup table from the Open Targets disease index.
-
-    Args:
-        disease_index (DataFrame): Open Targets disease index.
-
-    Returns:
-        DataFrame: Disease id lookup table.
-    """
-    return (
-        disease_index
-        # extract entities from relevant fields and annotate entity with score and nlpPipelineTrack
-        .select(
-            f.col("id").alias("entityId"),
-            annotate_entity(
-                f.array(f.col("id")), 1.0, "symbol"
-            ).alias("identifier"),
-            annotate_entity(
-                f.col("dbXRefs"), 0.999, "symbol"
-            ).alias("crossRefs"),
-            annotate_entity(
-                f.col("obsoleteXRefs"), 0.998, "symbol"
-            ).alias("obsoleteCrossRefs")
-        )
-        # flatten and explode array of structs
-        .withColumn(
-            "entity",
-            f.explode(
-                f.flatten(
-                    f.array(    
-                        f.col("identifier"),
-                        f.col("crossRefs"),
-                        f.col("obsoleteCrossRefs")
-                    )
-                )
-            )
-        )
-        # select relevant fields and specify entity type
-        .select(
-            f.col("entityId"),
-            f.upper(f.trim(f.col("entity.entityLabel"))).alias("entityLabel"),
-            f.col("entity.entityScore").alias("entityScore"),
-            f.col("entity.nlpPipelineTrack").alias("nlpPipelineTrack"),
-            f.lit("DS").alias("entityType")
-        )
-        # filter out disease crossrefs with irrelevant prefixes
-        .transform(filter_disease_crossrefs)
-        # format disease identifier to have consistent formatting
-        .withColumn("entityLabel", format_disease_identifier(f.col("entityLabel")))
-        # cleanup
         .filter((f.col("entityLabel").isNotNull()) & (f.length("entityLabel") > 0))
         .distinct()
     )
